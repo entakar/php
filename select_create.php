cf36f606a167700ce18dbf94cc20d18a548d80a6
takashi_endo <takasea007@gmail.com>
2015-01-15 23:09:23 +0900

    Update select_create.php

--- select_create.php
+++ select_create.php
@@ -6,16 +6,14 @@
 // $master_selectは多次元配列 初めは連番を振る
 /////////////////////////////////////////////////////////////////////////////////
 function select_bar($selected_item,$master_select){
-$textmassage = "<Select name=\"master\" >";
+$textmassage = "<Select name =\"master\" >";
 $master_count = count($master_select);
 for($x = 0;$x < $master_count ; $x++ ){
-if($selected_item == $master_select[$x][0]){
-$textmassage .= "<option name=\"" . $master_select[$x][0] . "\" selected=\"selected\" >";
-$textmassage .= $master_select[$x][0] ."</option>";
-}
-else{
-$textmassage .= "<option name=\"" . $master_select[$x][0] . "\"  >". $master_select[$x][0] ."</option>";
-}
+  $textmassage .= "< option name =\"" . $master_select[$x][0] ."\"";
+  if($selected_item == $master_select[$x][0]){
+  $textmassage .=  " selected=\"selected\" ";
+  }
+  $textmassage .= " >". $master_select[$x][0] ."</option>";
 }
 $textmassage .= "</select>";
 
